f687f32fb0f66fac2747e7d76565fbe51ca4cfba
RobertMaged <rmsr888@gmail.com>
2021-01-02 05:44:20 +0200

    service

--- app/src/main/java/z/com/android/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/z/com/android/photogallery/PhotoGalleryActivity.java
@@ -1,11 +1,18 @@
 package z.com.android.photogallery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    public static Intent newIntent(Context context){
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
 
     @Override
     public Fragment createFragment() {
--- app/src/main/java/z/com/android/photogallery/PollService.java
+++ app/src/main/java/z/com/android/photogallery/PollService.java
@@ -1,24 +1,57 @@
 package z.com.android.photogallery;
 
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.os.Build;
+import android.os.SystemClock;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.AlarmManagerCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class PollService extends IntentService {
     private static final String TAG = "PollService";
+    //set Interval to 1 min
+    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(15);
 
     public static Intent newIntent(Context context){
         return new Intent(context, PollService.class);
     }
 
     public static void setServiceAlarm(Context context, boolean isOn){
-        
+
+        Intent intent = PollService.newIntent(context);
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        if (isOn){
+            alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime(), POLL_INTERVAL_MS, pendingIntent);
+        } else {
+            alarmManager.cancel(pendingIntent);
+            pendingIntent.cancel();
+        }
+
+    }
+
+    public static boolean isServiceAlarmOn(Context context){
+        Intent intent = PollService.newIntent(context);
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_NO_CREATE);
+
+        return pendingIntent != null;
     }
 
     public PollService() {
@@ -50,6 +83,31 @@ public class PollService extends IntentService {
             Log.i(TAG, "Got an old result: " + resultId);
         } else {
             Log.i(TAG, "Got a new result: " + resultId);
+
+            Intent i = PhotoGalleryActivity.newIntent(this);
+            PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+
+
+            Notification notification;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel("info", "test", NotificationManager.IMPORTANCE_DEFAULT);
+
+                 notification = new Notification.Builder(this , channel.getId())
+                        .setTicker(getResources().getString(R.string.new_pictures_title))
+                        .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                        .setContentTitle(getResources().getString(R.string.new_pictures_title))
+                        .setContentText(getResources().getString(R.string.new_pictures_text))
+                        .setContentIntent(pi)
+                        .setAutoCancel(true)
+                        .build();
+
+                //NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                notificationManager.createNotificationChannel(channel);
+                notificationManager.notify(0,notification);
+            }
+
+
         }
 
         QueryPreferences.setLastResultId(this, resultId);
